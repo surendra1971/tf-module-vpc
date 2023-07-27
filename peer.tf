334717402e549b754dfa8531f9456b37cbd9bb3b
Your Name <you@example.com>
2023-07-27 23:14:59 +0530

    updating the Auto Push

---
+++ peer.tf
@@ -0,0 +1,11 @@
+# # Requester's side of the connection.
+resource "aws_vpc_peering_connection" "peer" {
+  
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.DEFAULT_VPC_ID
+  auto_accept   = true
+
+  tags = {
+    Side = "Robot-${var.ENV}-default-vpc-peering"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -3,3 +3,4 @@ variable "ENV" {}
 variable "AZ" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
+variable "DEFAULT_VPC_ID" {} 
