2aa40fbe361d10e7c58ecb0984d4e1c5e90b7b5b
Your Name <you@example.com>
2023-07-27 23:48:58 +0530

    updating the Auto Push

--- route-table.tf
+++ route-table.tf
@@ -46,3 +46,11 @@ resource "aws_route_table_association" "private_subnet_rt_association" {
   subnet_id             = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id        = aws_route_table.private_rt.id
 }
+
+# Adding route to the already existing default vpc RT
+resource "aws_route" "def_route_robot_vpc" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  depends_on                = [aws_vpc_peering_connection.peer]
+}
--- vars.tf
+++ vars.tf
@@ -5,5 +5,6 @@ variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "DEFAULT_VPC_ID" {} 
 variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
   
 
