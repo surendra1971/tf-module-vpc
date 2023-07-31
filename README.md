1850d57cf627e35aa23e42e08efb50d5d2fe71b7
Your Name <you@example.com>
2023-07-31 12:25:21 +0530

    updating the Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # tf-module-vpc
+
+Information of vpc id, subnetid, cidrs are important and are needed by other modules which are created remotely
+
+...
+
+So, how are we going to pass this information between two different statefiles.
+
+* Whatever the information that you would like to share, print that output.
+
+* Once you print something as an output, that information will also be recorded on the STATEFILE.
+
+* Once something is recored in the statefile, other resources can access it by using "terraform remote state datasource"
+
+...
---
+++ output.tf
@@ -0,0 +1,26 @@
+output "VPC_ID" {
+    value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+    value = aws_vpc.main.cidr_block
+}
+output "PRIVATE_SUBNET_IDS" {
+    value = aws_subnet.private_subnet.*.id
+}
+output "PUBLIC_SUBNET_IDS" {
+    value = aws_subnet.public_subnet.*.id
+}
+output "PRIVATE_SUBNET_CIDR" {
+    value = aws_subnet.private_subnet.*.cidr_block
+}
+output "PUBLIC_SUBNET_CIDR" {
+    value = aws_subnet.public_subnet.*.cidr_block
+}
+
+output "DEFAULT_VPC_ID" {
+    value  = var.DEFAULT_VPC_ID
+}
+output "DEFAULT_VPC_CIDR" {
+    value  =  var.DEFAULT_VPC_CIDR
+}
